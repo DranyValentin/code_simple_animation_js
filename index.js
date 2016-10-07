48f41f2d39a1ad9cea85bf887a859df7f3c4d004
Feel <dr.pin@mail.ru>
2016-10-07 18:10:51 +0300

    fix some mistake

--- index.js
+++ index.js
@@ -1,22 +1,24 @@
 (function()
 {
-  "use strict"
-  
-  var $el = document.querySelector('div') // Animated element
-  var currentPosition = 0
-  var step = 1 //Speed of motion element (Скорость перемещаемого объекта)
-  var requestAnimationFrameID = requestAnimationFrame(doAnim)
-  
-  function doAnim()
-	{
-		$el.setAttribute('style', 'transform: ' + currentPosition)
+	"use strict"
 
-		if ( step >= 200 ) // Statement, when need stop
+	console.log("File 'index.js' work")
+
+	var $el = document.querySelector('div') // Animated element
+	var currentPosition = 0
+	var step = 1 //Speed of motion element (Скорость перемещаемого объекта)
+	var requestAnimationFrameID = requestAnimationFrame(doAnim)
+
+	function doAnim()
+	{
+		$el.setAttribute('style', 'transform: translate(' + currentPosition + 'px, 0)')
+
+		if ( currentPosition >= 200 ) // Statement, when need stop
 		{
 		  cancelAnimationFrame(requestAnimationFrameID);
 	    return;
 		}
-    
+
 		currentPosition += step
 
 		requestAnimationFrameID = requestAnimationFrame(doAnim) // Next iteration
